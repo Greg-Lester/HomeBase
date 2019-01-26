85c31b0a3b31f12d52085d599cdc0e547d5f1042
Greg Lester <contactgreglester@gmail.com>
2019-01-26 10:00:27 +0000

    initial commit

---
+++ Home Base/Assets/Classes/BaseBuilder.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BaseBuilder : MonoBehaviour
+{
+    public GameObject WallPrefab;
+
+
+    // Use this for initialization
+    void Start()
+    {
+        Block[] testArray = new Block[]{
+            new Block(3, 1),
+            new Block(2, 3),
+            new Block(1, 4),
+            new Block(5, 2),
+            new Block(4, 6)
+        };
+        this.BuildBase(testArray);
+    }
+
+   void BuildBase(Block[] blocks){
+        foreach(Block b in blocks)
+            Instantiate(WallPrefab, new Vector3(b.x, b.y, 0), Quaternion.identity);
+        
+    }
+
+}
---
+++ Home Base/Assets/Classes/Block.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Block {
+    public int x;
+    public int y;
+
+    public Block(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+}
