7b279996569be3cbe1208ae6fef7c56e2193f932
Greg Lester <contactgreglester@gmail.com>
2019-01-26 11:36:28 +0000

    bullet system where blocks dissapear

---
+++ Home Base/Assets/Classes/Bullet.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+    void Update() {}
+    void OnCollisionEnter2D(Collision2D col) {
+ 
+ 
+        {
+            //Check collision name
+            Debug.Log("collision name = " + col.gameObject.name);
+            if (col.gameObject.name == "Block(Clone)")
+            {
+                Destroy(col.gameObject);
+                Destroy(this.gameObject);
+            }
+        }
+    }
+}
---
+++ Home Base/Assets/Classes/CannonShoot.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CannonShoot : MonoBehaviour
+{
+    public GameObject ProjectilePrefab;
+
+
+    // Use this for initialization
+    void Start()
+    {
+        Shoot(0, 0, 6, 8);
+    }
+
+    void Shoot(float x, float y, float dx, float dy)
+    {
+        GameObject cannonball = Instantiate(ProjectilePrefab, new Vector3(x, y, 0), Quaternion.identity);
+        Rigidbody2D rb = cannonball.GetComponent<Rigidbody2D>();
+        rb.velocity = new Vector3(dx, dy, 0);
+    }
+
+}
