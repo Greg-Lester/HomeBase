2843a7be26a98f2ee0829172f381142cc070850f
Kai Salmon <kaisalmon@hotmail.com>
2019-01-26 11:55:39 +0000

    Added AirConsole stuff

---
+++ Home Base/Assets/Classes/AirConsolerManager.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using NDream.AirConsole;
+using Newtonsoft.Json.Linq;
+using UnityEngine;
+
+public class AirConsolerManager : MonoBehaviour
+{
+    void Start()
+    {
+        AirConsole.instance.onMessage += OnMessage;
+    }
+
+    void OnMessage(int from, JToken data)
+    {
+        float dx = (float)data["info"]["dx"];
+        float dy = (float)data["info"]["dy"];
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
