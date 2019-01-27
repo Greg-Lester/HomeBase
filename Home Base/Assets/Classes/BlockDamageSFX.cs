bd31dcd729d2d8385ddec3407a7a23fad0b1c485
Greg Lester <contactgreglester@gmail.com>
2019-01-27 13:37:07 +0000

    spawn block sounds

---
+++ Home Base/Assets/Classes/BlockDamageSFX.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BlockDamageSFX : MonoBehaviour
+{
+    private AudioSource audioSource;
+    public AudioClip[] damagesfx;
+    private AudioClip damagesfxClip;
+    void Start()
+    {
+        audioSource = gameObject.GetComponent<AudioSource>();
+    }
+    public void PlaySoundeffects()
+    {
+        int index = Random.Range(0, damagesfx.Length);
+        damagesfxClip = damagesfx[index];
+        audioSource.clip = damagesfxClip;
+        audioSource.Play();
+    }
+    void Update()
+    {
+
+    }
+}
